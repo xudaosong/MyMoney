9b14cdfd37f516adca0ed26b6387848202fd39a1
xudaosong <xudaosong@163.com>
2016-01-19 19:15:51 +0800

    add default route

--- web/src/login/login.controller.js
+++ web/src/login/login.controller.js
@@ -1,23 +1,25 @@
-(function () {
+(function() {
     'use strict';
     angular
         .module('money.login')
         .controller('LoginController', LoginController);
-    LoginController.$inject = ['$scope', 'Restangular', '$window','authentication','$state'];
+    LoginController.$inject = ['$scope', 'Restangular', '$window', 'authentication', '$state'];
     /* @ngInject */
-    function LoginController($scope, Restangular, $window,authentication,$state) {
+    function LoginController($scope, Restangular, $window, authentication, $state) {
         var vm = this,
             login = Restangular.all('login');
         vm.doLogin = doLogin;
         ////////////////////////////
-       
+
         function doLogin(user) {
-            login.post(user).then(function (res) {
+            login.post(user).then(function(res) {
                 authentication.user = res;
                 $window.sessionStorage.user = angular.toJson(res);
-                Restangular.setDefaultHeaders({Authorization: "Bearer " + res.token});
-                $state.go('checklist');
-            }, function (res) {
+                Restangular.setDefaultHeaders({
+                    Authorization: "Bearer " + res.token
+                });
+                $state.go('home');
+            }, function(res) {
                 alert(res.data.message);
             });
         }
--- web/src/money.web.js
+++ web/src/money.web.js
@@ -1,29 +1,20 @@
-//(function () {
-//    'use strict';
-//
-//    angular
-//        .module('money', ['ui.router', 'fish', 'money.views'])
-//        .config(function ($locationProvider,$stateProvider,$urlRouterProvider) {
-//            $locationProvider.hashPrefix('!');
-//
-//            $stateProvider.
-//                state('home',{
-//                    url:'/',
-//                    templateUrl:'home/home.html',
-//                    controller:'HomeCtrl'
-//                })
-//                .state('about', {
-//                    url:'/about',
-//                    templateUrl: 'about/about.html',
-//                    controller: 'AboutCtrl'
-//                });
-//            $urlRouterProvider.otherwise('/');
-//        });
+
+(function () {
+  'use strict';
+  angular
+    .module('money')
+    .run(run);
+
+  run.$inject = ['routerHelper'];
+
+  function run(routerHelper) {
+    routerHelper.configureStates([], '/');
     // 解决Facebook身份验证后跳转的Bug。
     // 在OAuth来回的调用中，会在URL的#后面增加字符
-    //if (window.location.hash === '#_=_') window.location.hash = '#!';
-    //
-    //angular.element(document).ready(function () {
+    // if (window.location.hash === '#_=_') window.location.hash = '#!';
+    
+    // angular.element(document).ready(function () {
     //    angular.bootstrap(document, ['money']);
-    //});
-//})();
+    // });
+  }
+})();
