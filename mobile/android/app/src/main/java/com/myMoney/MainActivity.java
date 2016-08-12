5e8278a1870531b94865219e5f744d639f9f6bdd
803850@nd <xudaosong@163.com>
2016-08-12 18:57:47 +0800

    add webviewbridge

--- mobile/android/app/src/main/java/com/myMoney/MainActivity.java
+++ mobile/android/app/src/main/java/com/myMoney/MainActivity.java
@@ -1,7 +1,12 @@
 package com.myMoney;
 
 import com.facebook.react.ReactActivity;
+import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 
+import java.util.Arrays;
+import java.util.List;
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +16,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "myMoney";
+    }
+
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new WebViewBridgePackage()
+        );
     }
 }
