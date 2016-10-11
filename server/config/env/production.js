4db5057583d64155ea6707b809954e7486ba764a
xudaosong <xudaosong@163.com>
2016-10-11 21:49:42 +0800

    修改mongodb的产品连接地址

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     secret: 'productSessionSecret',
-    db: 'mongodb://localhost/mymoney',
+    db: 'mongodb://xudaosong:xds8504@ds055626.mlab.com:55626/mymoney',
     staticUrl: '//s1.money.com',
     static:'../web/dist',
     port:'6000',
